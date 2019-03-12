0bf63a363ecb506f09bb7f0361993d3936c29815
Amrit Singh <Amrit.Singh@hli.ubc.ca>
2019-03-12 12:02:49 -0400

    add coordinate and intset sols

--- week5.py
+++ week5.py
@@ -63,6 +63,98 @@ Y = Y + w2.getY()
 print(Y) # int, 44
 print(w2.getY()) # int, 8
 
+# Exercise: coordinate
+class Coordinate(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def getX(self):
+        # Getter method for a Coordinate object's x coordinate.
+        # Getter methods are better practice than just accessing an attribute directly
+        return self.x
+
+    def getY(self):
+        # Getter method for a Coordinate object's y coordinate
+        return self.y
+
+    def __str__(self):
+        return '<' + str(self.getX()) + ',' + str(self.getY()) + '>'
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+    
+    def __repr__(self):
+        return 'Coordinate(' + str(self.x) + ',' + str(self.y) + ')' 
+
+c = Coordinate(2,-8)
+b = Coordinate(2,-8)
+print(c)
+eval(repr(c))
+c == b
+
+# Exercise: int set
+class intSet(object):
+    """An intSet is a set of integers
+    The value is represented by a list of ints, self.vals.
+    Each int in the set occurs in self.vals exactly once."""
+
+    def __init__(self):
+        """Create an empty set of integers"""
+        self.vals = []
+
+    def insert(self, e):
+        """Assumes e is an integer and inserts e into self""" 
+        if not e in self.vals:
+            self.vals.append(e)
+
+    def member(self, e):
+        """Assumes e is an integer
+           Returns True if e is in self, and False otherwise"""
+        return e in self.vals
+
+    def remove(self, e):
+        """Assumes e is an integer and removes e from self
+           Raises ValueError if e is not in self"""
+        try:
+            self.vals.remove(e)
+        except:
+            raise ValueError(str(e) + ' not found')
+
+    def __str__(self):
+        """Returns a string representation of self"""
+        self.vals.sort()
+        return '{' + ','.join([str(e) for e in self.vals]) + '}'
+
+    def intersect(self, other):
+        """Return a new intSet containing the common elements"""
+        self.com = []
+        for e in self.vals:
+            if e in other.vals:
+                self.com.append(e)
+        return '{' + ','.join([str(i) for i in self.com]) + '}' 
+        
+    def __len__(self):
+        return len(self.vals)
+
+e = intSet()
+e.insert(1)
+e.insert(2)
+e.insert(3)
+print(e)
+len(e)
+f = intSet()
+f.insert(4)
+f.insert(2)
+f.insert(1)
+e.intersect(f)
+
+
+
+
+
+
+
 
 
 
